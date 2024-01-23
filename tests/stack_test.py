7ea4ac66c95935787dac79ce5feb4390fb09608a
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2024-01-23 15:18:07 +0200

    add first tests for stack data structure :tada:

---
+++ tests/stack_test.py
@@ -0,0 +1,49 @@
+import unittest
+from src.classes.stack import Stack
+
+class TestStack(unittest.TestCase):
+    def setUp(self) -> None:
+        self.stack = Stack()
+    
+    def test_push(self) -> None:
+        self.stack.push("+1")
+        self.stack.push("+2")
+        self.assertEqual(self.stack.peek(), "+2")
+        self.stack.push("+3")
+        self.assertEqual(self.stack.peek(), "+3")
+
+    def test_pop(self) -> None:
+        self.stack.push("+1")
+        self.stack.push("+2")
+        self.stack.push("+3")
+        self.stack.pop()
+        self.assertEqual(self.stack.peek(), "+2")
+        self.stack.pop()
+        self.assertEqual(self.stack.peek(), "+1")
+
+    def test_peek(self) -> None:
+        self.stack.push("+1")
+        self.stack.push("+2")
+        self.assertEqual(self.stack.peek(), "+2")
+        self.stack.push("+3")
+        self.assertEqual(self.stack.peek(), "+3")
+
+    def test_is_empty(self) -> None:
+        self.assertTrue(self.stack.is_empty())
+        self.stack.push("+1")
+        self.assertFalse(self.stack.is_empty())
+
+    def test_size(self) -> None:
+        self.assertEqual(self.stack.size(), 0)
+        self.stack.push("+1")
+        self.assertEqual(self.stack.size(), 1)
+        self.stack.push("+2")
+        self.assertEqual(self.stack.size(), 2)
+        self.stack.push("+3")
+        self.assertEqual(self.stack.size(), 3)
+        self.stack.pop()
+        self.assertEqual(self.stack.size(), 2)
+        self.stack.pop()
+        self.assertEqual(self.stack.size(), 1)
+        self.stack.pop()
+        self.assertEqual(self.stack.size(), 0)
