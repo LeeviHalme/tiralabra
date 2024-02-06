00f74d931a88006b60d0129bd52e4938c8daab42
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2024-02-06 18:04:04 +0200

    feat: add test for invalid parenthesis count

--- tests/shunting_yard_basic_test.py
+++ tests/shunting_yard_basic_test.py
@@ -1,5 +1,5 @@
 import unittest
-from src.classes.shunting_yard import ShuntingYard
+from src.classes.shunting_yard import ShuntingYard, MalformedExpressionException
 
 class TestBasicShuntingYard(unittest.TestCase):
     def setUp(self) -> None:
@@ -9,3 +9,8 @@ class TestBasicShuntingYard(unittest.TestCase):
         self.assertEqual(
             self.shunting_yard.parse("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3"), \
                 "3 4 2 * 1 5 - 2 3 ^ ^ / +")
+
+    # test invalid parentehsis count
+    def test_invalid_parenthesis(self):
+        with self.assertRaises(MalformedExpressionException):
+            self.shunting_yard.parse("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3 )")
