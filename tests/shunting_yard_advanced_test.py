490922111aabc880c2e3f569fdfaa4b8866f6992
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2024-02-25 23:13:37 +0200

    fix: remove division by zero test from shunting yard (->rpn evaluator)

--- tests/shunting_yard_advanced_test.py
+++ tests/shunting_yard_advanced_test.py
@@ -5,7 +5,7 @@ from src.classes.exceptions import MalformedExpressionException
 class TestAdvancedShuntingYard(unittest.TestCase):
     def setUp(self) -> None:
         self.shunting_yard = ShuntingYard()
-    
+
     def test_empty_expression(self):
         with self.assertRaises(MalformedExpressionException):
             self.shunting_yard.parse("")
@@ -13,7 +13,3 @@ class TestAdvancedShuntingYard(unittest.TestCase):
     def test_mismatched_parentheses(self):
         with self.assertRaises(MalformedExpressionException):
             self.shunting_yard.parse("5 + )6 * 2")
-
-    # def test_division_by_zero(self):
-    #     with self.assertRaises(MalformedExpressionException):
-    #         self.shunting_yard.parse("5 / 0")
